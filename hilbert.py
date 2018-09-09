b7fc484a155138e0323b2c5d98569438449c105b
Gabriel <gabriel.altay@gmail.com>
2018-09-09 14:31:18 -0400

    more checking of input

--- hilbert.py
+++ hilbert.py
@@ -9,54 +9,7 @@ We consider an N-dimensional `hypercube`_ of side length :math:`2^p`.
 This hypercube contains :math:`2^{N p}` unit hypercubes (:math:`2^p` along
 each dimension).  The number of unit hypercubes determine the possible
 discrete distances along the Hilbert curve (indexed from :math:`0` to
-:math:`2^{N p} - 1`).  The image below illustrates the situation for
-:math:`N=2` and :math:`p=3`.
-
-.. figure:: nD=2_p=3.png
-
-   This is the third iteration (:math:`p=3`) of the Hilbert curve in two
-   (:math:`N=2`) dimensions.  Distances, :math:`h`, along the curve are
-   labeled from 0 to 63 (i.e. from 0 to :math:`2^{N p}-1`).  The provided
-   functions translate between N-dimensional coordinates and the one
-   dimensional distance.  For example, between (:math:`x_0=4, x_1=6`) and
-   :math:`h=36`.
-
-Reference
-=========
-
-This module is based on the C code provided in the 2004 article
-"Programming the Hilbert Curve" by John Skilling,
-
-  - http://adsabs.harvard.edu/abs/2004AIPC..707..381S
-
-I was also helped by the discussion in the following stackoverflow post,
-
-  - `mapping-n-dimensional-value-to-a-point-on-hilbert-curve`_
-
-which points out a typo in the source code of the paper.  The Skilling code
-provides two functions ``TransposetoAxes`` and ``AxestoTranspose``.  In this
-case, Transpose refers to a specific packing of the integer that represents
-distance along the Hilbert curve (see below for details) and
-Axes refer to the N-dimensional coordinates.  Below is an excerpt of the docs
-from that code that appears in the paper by Skilling, ::
-
-//+++++++++++++++++++++++++++ PUBLIC-DOMAIN SOFTWARE ++++++++++++++++++++++++++
-// Functions: TransposetoAxes  AxestoTranspose
-// Purpose:   Transform in-place between Hilbert transpose and geometrical axes
-// Example:   b=5 bits for each of n=3 coordinates.
-//            15-bit Hilbert integer = A B C D E F G H I J K L M N O is stored
-//            as its Transpose
-//                   X[0] = A D G J M                X[2]|
-//                   X[1] = B E H K N    <------->       | /X[1]
-//                   X[2] = C F I L O               axes |/
-//                          high  low                    0------ X[0]
-//            Axes are stored conveniently as b-bit integers.
-// Author:    John Skilling  20 Apr 2001 to 11 Oct 2003
-
-
-.. _Hilbert curve: https://en.wikipedia.org/wiki/Hilbert_curve
-.. _hypercube: https://en.wikipedia.org/wiki/Hypercube
-.. _mapping-n-dimensional-value-to-a-point-on-hilbert-curve: http://stackoverflow.com/questions/499166/mapping-n-dimensional-value-to-a-point-on-hilbert-curve/10384110#10384110
+:math:`2^{N p} - 1`).
 """
 
 
@@ -75,8 +28,18 @@ class HilbertCurve:
             p (int): iterations to use in the hilbert curve
             n (int): number of dimensions
         """
+        if p <= 0:
+            raise ValueError('p must be > 0')
+        if n <= 0:
+            raise ValueError('n must be > 0')
         self.p = p
         self.n = n
+
+        # maximum distance along curve
+        self.max_h = 2**(self.p * self.n) - 1
+
+        # maximum coordinate value in any dimension
+        self.max_x = 2**self.p - 1
 
     def _hilbert_integer_to_transpose(self, h):
         """Store a hilbert integer (`h`) as its transpose (`x`).
@@ -116,9 +79,10 @@ class HilbertCurve:
             x (list): transpose of h
                       (n components with values between 0 and 2**p-1)
         """
-        max_h = 2**(self.p * self.n) - 1
-        if h > max_h:
-            raise ValueError('h={} is greater than 2**(p*N)-1={}'.format(h, max_h))
+        if h > self.max_h:
+            raise ValueError('h={} is greater than 2**(p*N)-1={}'.format(h, self.max_h))
+        if h < 0:
+            raise ValueError('h={} but must be > 0'.format(h))
 
         x = self._hilbert_integer_to_transpose(h)
         Z = 2 << (self.p-1)
@@ -161,11 +125,15 @@ class HilbertCurve:
         if len(x) != self.n:
             raise ValueError('x={} must have N={} dimensions'.format(x, self.n))
 
-        max_x = 2**self.p - 1
-        if any(elx > max_x for elx in x):
+        if any(elx > self.max_x for elx in x):
             raise ValueError(
                 'invalid coordinate input x={}.  one or more dimensions have a '
-                'value greater than 2**p-1={}'.format(x, max_x))
+                'value greater than 2**p-1={}'.format(x, self.max_x))
+
+        if any(elx < 0 for elx in x):
+            raise ValueError(
+                'invalid coordinate input x={}.  one or more dimensions have a '
+                'value less than 0'.format(x))
 
         M = 1 << (self.p - 1)
 
