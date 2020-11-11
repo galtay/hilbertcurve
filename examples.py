0351a67f25d22a4d3c1086f777b2d72d7b8c2051
Gabriel Altay <gabriel.altay@gmail.com>
2020-11-11 17:13:19 -0500

    extended type hints to floats (#30)

--- examples.py
+++ examples.py
@@ -11,30 +11,30 @@ from hilbertcurve.hilbertcurve import HilbertCurve
 
 # calculate distances along a hilbert curve given coordinates
 p = 1
-N = 2
-hilbert_curve = HilbertCurve(p, N)
+n = 2
+hilbert_curve = HilbertCurve(p, n)
 for coords in [[0,0], [0,1], [1,1], [1,0]]:
     dist = hilbert_curve.distance_from_coordinates(coords)
     print(f'distance(x={coords}) = {dist}')
 
 # calculate coordinates given distances along a hilbert curve
 p = 1
-N = 2
-hilbert_curve = HilbertCurve(p, N)
+n = 2
+hilbert_curve = HilbertCurve(p, n)
 for ii in range(4):
-    print('coords(h={},p={},N={}) = {}'.format(
-        ii, p, N, hilbert_curve.coordinates_from_distance(ii)))
+    print('coords(h={},p={},n={}) = {}'.format(
+        ii, p, n, hilbert_curve.coordinates_from_distance(ii)))
 
 
 # due to the magic of arbitrarily large integers in
 # Python (https://www.python.org/dev/peps/pep-0237/)
 # these calculations can be done with absurd numbers
 p = 512
-N = 10
-hilbert_curve = HilbertCurve(p, N)
+n = 10
+hilbert_curve = HilbertCurve(p, n)
 ii = 123456789101112131415161718192021222324252627282930
 coords = hilbert_curve.coordinates_from_distance(ii)
-print('coords(h={},p={},N={}) = {}'.format(ii, p, N, coords))
+print('coords(h={},p={},n={}) = {}'.format(ii, p, n, coords))
 
 
 coords = [121075, 67332, 67326, 108879, 26637, 43346, 23848, 1551, 68130, 84004]
